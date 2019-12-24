3765e6919e5ad2e0adc215dd6e92d56e226b5a95
inasakunovel <inasaku.novel@gmail.com>
2019-12-24 15:24:33 +0900

    Remove: 不要なコメント削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,12 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
   def download_and_attach_avatar(filename)
-    binding.pry
     filename = filename&.gsub(/_normal/, '')
     return unless filename
 
     file = open(filename)
-    binding.pry
     avatar.attach(io: file,
                   filename: "avatar.#{file.content_type_parse.first.split("/").last}",
                   content_type: file.content_type_parse.first)
-    binding.pry
   end
 end
