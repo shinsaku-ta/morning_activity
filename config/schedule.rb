8136baf4e966040c9d00470a3b083d0333939075
inasakunovel <inasaku.novel@gmail.com>
2020-01-14 17:47:15 +0900

    Fix: wheneverの時間変更

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,7 +8,6 @@ set :environment, rails_env
 # cronのログの吐き出し場所。ここでエラー内容を確認する
 set :output, "#{Rails.root}/log/cron.log"
 
-# every 1.day, at: '12 am' do
-every 1.day, at: '5:41 pm' do
+every 1.day, at: '12 am' do
   rake 'check_state:auto_tweet'
 end
