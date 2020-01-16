5b99bc42ab428fb1576a9de86a4d68451cd874c0
inasakunovel <inasaku.novel@gmail.com>
2020-01-16 21:43:26 +0900

    Fix: Twitter変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  host = 'samplehost'
+  host = 'database-1.cnnbuq4uwp1g.ap-northeast-1.rds.amazonaws.com'
   Rails.application.routes.default_url_options[:host] = host
   # Code is not reloaded between requests.
   config.cache_classes = true
